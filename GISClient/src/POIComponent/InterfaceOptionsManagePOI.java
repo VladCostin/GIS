eb4dee7cb2dc27bace02f4a9d17534bbd159f41d
Vlad <vlad.costin_h>
2014-11-07 01:10:52 +0200

    adding the constants used in the interface POI Component

---
+++ GISClient/src/POIComponent/InterfaceOptionsManagePOI.java
@@ -0,0 +1,19 @@
+package POIComponent;
+
+/**
+ * contains constants used in the panel to manage the POI Component
+ * @author Vlad Herescu
+ *
+ */
+public interface InterfaceOptionsManagePOI {
+
+	String optionAdd	= "Add POI";
+	
+	String optionSelect = "Select POI";
+	
+	String optionDelete = "Delete POI";
+	
+	String buttonLoad = "Load the POI into the map";
+	
+	
+}
